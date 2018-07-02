b53d7966c7b6d4f45b46f3b03d34f934bb141e09
weibinl <39772822+weibinl@users.noreply.github.com>
2018-07-02 11:20:35 -0400

    Add files via upload
    mesh ready(physical control, auto)

--- COMSOL_DLD_2018_7_2.m
+++ COMSOL_DLD_2018_7_2.m
@@ -3,10 +3,11 @@ clear all
 clc
 import com.comsol.model.*
 import com.comsol.model.util.*
+%% start of geometry generation 
 %array generation 
 a=1;
 model = ModelUtil.create('Model');
-model.component.create('comp1',true);
+comp1=model.component.create('comp1',true);
 geom1 = model.component('comp1').geom.create('geom1',3);
 wp1 = geom1.feature.create('wp1', 'WorkPlane');
 wp1.set('planetype', 'quick');
@@ -22,8 +23,8 @@ wp2 = geom1.feature.create('wp2', 'WorkPlane');
 wp2.set('planetype', 'quick');
 wp2.set('quickplane', 'xy');
 array=wp2.geom.selection().create("array","CumulativeSelection");
-for a=1:8
-    for b=1:8
+for a=1:2
+    for b=1:2
 wp2.geom.feature.create("i1"+a+b,'Circle');
 wp2.geom.feature("i1"+a+b).set('r',2);
 wp2.geom.feature("i1"+a+b).set('pos',[a*10 b*10]);
@@ -39,14 +40,22 @@ geom1.run
 ext2 = geom1.feature.create('ext2', 'Extrude');
 ext2.set('distance', '10');  %extrude the post and the boundary
 %cut the simulation by the array 
-dif1 = geom1.feature.create('dif1', 'Difference');
-dif1.selection('input').set({'ext1'});
-dif1.selection('input2').set({'ext2'});
+FlowChannel = geom1.feature.create('FlowChannel', 'Difference');
+FlowChannel.selection('input').set({'ext1'});
+FlowChannel.selection('input2').set({'ext2'});
 geom1.run;
 mphgeom(model);
-%finish the 3D geometry generation 
-% save to mph file in the following destination with file name 'test.mph'
+% end of 3D geometry generation 
 
-model.save('G:\test')
+%% start of mesh generation
+mesh1 = comp1.mesh.create('mesh1');
+model.component('comp1').mesh('mesh1').automatic(true);
+model.component('comp1').mesh('mesh1').autoMeshSize(3);
+mesh1.run;
+mphmesh(model)
+%% start of file saving
+% save to mph file in the following destination with file name 'test.mph'
+% 
+% model.save('G:\test')
 
 
